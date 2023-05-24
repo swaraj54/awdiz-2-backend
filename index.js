6cd3fade753373d90433012f9fc7966cdcd58b74
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-05-24 11:16:16 +0530

    updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import { kunal, Swaraj, Anu, Krishnan, Abhi } from './controllers/All-Controllers.js';
 import router from './routes/UserRoutes.js';
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -9,16 +10,10 @@ app.use(morgan('dev')); // use() - middleware
 app.use(express.json()); // data to parse
 app.use('/api/v1', router);
 
-// app.get('/kunal', kunal);
-// app.get('/anu', Anu);
-// app.get('/abhi', Abhi);
-// app.get('/kirshna', Krishnan);
-// app.get('/swaraj', Swaraj); // pass two parameters, first is path, second is function
-// app.post();
-// app.patch();
-// app.put();
-// app.delete();
 
+mongoose.connect('mongodb+srv://swaraj1920:swaraj1920@cluster0.6yd9l.mongodb.net/awdizDB?retryWrites=true&w=majority')
+.then(() => console.log("DB connected"))
+.catch((err) => console.log("DB Error => ", err));
 
 
 app.listen(8001, () => console.log("Working on port 8001")); // port
